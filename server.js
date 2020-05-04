29b576ac1d5d0b5f7d3677923e076857f7fddfc8
iconma <shyam@iconma.com>
2020-05-04 22:51:05 +0530

    intial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(__dirname + "/dist/shopizer"));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
 
 app.get("/*", function(req, rest) {
   resizeBy.sendFile(path.join(__dirname + "/dist/index.html"));
